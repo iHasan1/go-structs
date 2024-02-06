dd9d2cf3b5f44326e646a4ff83819ef026acde32
Imran Hasan <imran.h94@gmail.com>
2024-02-06 14:54:06 +0500

    created a user struct to store fname, lname and bDate

--- structs.go
+++ structs.go
@@ -1,5 +1,47 @@
 package main
 
+/*
+structs are similar to classes in other languages.
+They allow to group together related data and functions that can be passed as a single unit.
+*/
+
+import (
+	"fmt"	
+	"time"
+)
+
+type user struct {
+	firstName string
+	lastName string
+	birthdate string
+	createdAt time.Time
+}
+
 func main() {
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	var appUser user
 	
+	// struct literal notation
+	appUser = user{
+		firstName: userFirstName,
+		lastName: userLastName,
+		birthdate: userBirthdate,
+		createdAt: time.Now(),
+	}
+
+	outputUserDetails(appUser)
+}
+
+func outputUserDetails(u user) {
+	fmt.Println(u.firstName, u.lastName, u.birthdate, u.createdAt)
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+	fmt.Scanln(&value)
+	return value
 }
